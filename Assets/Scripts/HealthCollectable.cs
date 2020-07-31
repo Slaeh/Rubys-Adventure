10da81381d1564b454a75469bf77db841a277d06
Slaeh <harjitliyal@gmail.com>
2020-07-30 20:25:47 -0400

    Added health scripts

---
+++ Assets/Scripts/HealthCollectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectable : MonoBehaviour
+{
+    void onTriggerEnter2D(Collider2D other){
+        ControlRuby controller = other.GetComponent<ControlRuby>();
+
+        if(controller != null){
+            if(controller.health < controller.maxHealth){
+            controller.changeHealth(1);
+            Destroy(gameObject);
+            }
+        }
+    }
+}
